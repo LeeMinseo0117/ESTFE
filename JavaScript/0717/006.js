76d57c8588cba462dba870691f60e40931288c1e
LeeMinseo0117 <leems3164@naver.com>
2024-07-17 23:46:26 +0900

    0717 수정

---
+++ JavaScript/0717/006.js
@@ -0,0 +1,8 @@
+const cont = document.querySelector(".cont");
+console.log(cont.firstElementChild); // 첫번째 자식을 찾음
+console.log(cont.lastElementChild); // 마지막 자식을 찾음
+console.log(cont.nextElementSibling); // 다음 형제요소를 찾음
+console.log(cont.previousSibling); // 이전 형제노드를 찾음
+console.log(cont.children); // 모든 자식요소를 찾음
+console.log(cont.childNodes); // 모든 자식노드를 찾음
+console.log(cont.parentElement); // 부모 요소를 찾음
