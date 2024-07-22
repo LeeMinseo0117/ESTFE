e6f7fb5ebd853a80d583cf0ef3e80326105904ab
LeeMinseo0117 <leems3164@naver.com>
2024-07-22 09:21:50 +0900

    0721 작업 추가

---
+++ JavaScript/0719/003.js
@@ -0,0 +1,18 @@
+let myArr = new Array(1, 2, 3);
+let myArr2 = new Array(4, 5, 6);
+
+myArr2.length;
+myArr.length;
+
+myArr.forEach((item) => {
+  console.log(item);
+});
+
+myArr2.forEach((item) => {
+  console.log(item);
+});
+
+function Robot() {}
+let 옵티머스프라임 = new Robot();
+
+console.log(옵티머스프라임 instanceof Robot);
