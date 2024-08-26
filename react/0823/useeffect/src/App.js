0158dd8c4c21a85f7b24c372ace7bc938686f47c
LeeMinseo0117 <leems3164@naver.com>
2024-08-26 09:33:27 +0900

    내용 수정

--- react/0823/useeffect/src/App.js
+++ react/0823/useeffect/src/App.js
@@ -1,38 +1,24 @@
-import React, { useState, useEffect } from "react"; //useEffect추가
-
-function Counter() {
-  const [count, setCount] = useState(0);
-  const countUp = () => {
-    setCount(count + 1);
-  };
-  //count가 변했을때 동작할 행동을 useEffect를 이용해 구현
-  useEffect(() => {
-    if (count % 2) {
-      console.log("홀수입니다");
-    } else if (count !== 0 || count % 2 === true) {
-      console.log("짝수입니다");
-    }
-  }, [count]);
-  return (
-    <>
-      <div>{count}</div>
-      <button onClick={countUp}>up!</button>
-    </>
-  );
-}
+import React, { useState, useEffect } from "react";
 
 function Time(props) {
   const [today, setToday] = useState(new Date());
   const hour = today.getHours();
   const min = today.getMinutes();
   const sec = today.getSeconds();
-  console.log("렌더링이 됩니다..?"); //렌더링이 잘 되는지 확인용! 꼭 넣고 진행해주세요.
+
+  console.log("렌더링이 됩니다..?");
 
   useEffect(() => {
-    if (today !== setToday) {
-      console.log("시간 변경");
-    }
+    const intervalId = setInterval(() => {
+      const t = new Date();
+      setToday(t);
+    }, 1000);
+    return () => {
+      // clearInterval : setInterval 함수의 작동을 중단합니다.
+      clearInterval(intervalId);
+    };
   }, [today]);
+
   return (
     <div>
       <h1>
@@ -41,10 +27,10 @@ function Time(props) {
     </div>
   );
 }
+
 function App() {
   return (
     <div>
-      <Counter />
       <Time />
     </div>
   );
