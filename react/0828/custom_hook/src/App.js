bd50f9b740a1facb5fe567d37a918e53922aa451
LeeMinseo0117 <leems3164@naver.com>
2024-08-29 16:22:41 +0900

    중간 내용 추가

--- react/0828/custom_hook/src/App.js
+++ react/0828/custom_hook/src/App.js
@@ -1,22 +1,40 @@
-import InputComponent from "./InputComponent";
-import SomethingComponent from "./SomethingComponent";
 import useScroll from "./Hook/useScroll";
 import { useEffect, useState } from "react";
 import ImageList from "./components/ImageList";
 import Loading from "./components/Loading.jsx";
 
 function App() {
+  // 이미지 데이터 상태 관리
   const [imageList, setImageList] = useState([]);
 
+  // 로딩 상태 관리
   const [isLoading, setIsLoading] = useState(false);
 
-  const dataCount = "5";
+  // 불러올 데이터의 페이지 숫자 관리
+  const [pageToFetch, setPageToFetch] = useState(1);
+  // const pageToFetch = useRef(1);
 
-  async function fetchImages() {
+  const isBottom = useScroll();
+
+  useEffect(() => {
+    if (isBottom) {
+      // 다음 사진들을 다운받으세요.
+      console.log("다음 사진들을 다운받으세요.");
+      setPageToFetch((prevPage) => {
+        return prevPage + 1;
+      });
+    }
+  }, [isBottom]);
+
+  useEffect(() => {
+    fetchImages(pageToFetch);
+  }, [pageToFetch]);
+
+  async function fetchImages(pageNumber) {
     setIsLoading(true);
     try {
       const response = await fetch(
-        `https://picsum.photos/v2/list?page=1&limit=${dataCount}`
+        `https://picsum.photos/v2/list?page=${pageNumber}&limit=5`
       );
 
       if (!response.ok) {
@@ -25,7 +43,7 @@ function App() {
 
       const datas = await response.json();
 
-      setImageList(datas);
+      setImageList((prevImageList) => [...prevImageList, ...datas]);
       setIsLoading(false);
     } catch (error) {
       console.error("네트워크 통신에 문제가 있습니다.", error);
@@ -33,12 +51,8 @@ function App() {
     }
   }
 
-  useEffect(() => {
-    fetchImages();
-  }, []);
-
   return (
-    <div style={{ height: "2000px" }}>
+    <div>
       {isLoading && <Loading />}
       <ImageList imageList={imageList} />
     </div>
