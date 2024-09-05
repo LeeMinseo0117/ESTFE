51741f36d97a9d9ff3bb6fd6b72dce5ba0e4430a
LeeMinseo0117 <leems3164@naver.com>
2024-09-05 17:07:15 +0900

    내용 수정

--- webpack/0904/webpackTest/webpack.config.js
+++ webpack/0904/webpackTest/webpack.config.js
@@ -1,3 +1,5 @@
+const childProcess = require("child_process");
+
 const path = require("path");
 
 module.exports = {
@@ -24,4 +26,20 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new webpack.BannerPlugin({
+      //toLocaleString : 날짜의 문자열 표현을 지역의 언어에 맞는 형식으로 반환합니다.
+      banner: `'마지막 빌드 시간은 ' + new Date().toLocaleString() + ' 입니다.'
+      Commit version : ${childProcess.execSync("git rev-parse --short HEAD")}
+    Committer : ${childProcess.execSync("git config user.name")}
+    Commit Date : ${new Date().toLocaleString()}`,
+    }),
+    new webpack.DefinePlugin({
+      pw: 123456,
+    }),
+  ],
+  banner: `
+    Commit version : ${childProcess.execSync("git rev-parse --short HEAD")}
+    Committer : ${childProcess.execSync("git config user.name")}
+    Commit Date : ${new Date().toLocaleString()}`,
 };
