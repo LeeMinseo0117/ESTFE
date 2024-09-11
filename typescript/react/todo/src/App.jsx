29a2bfc6e73fa3438d814a705044229c21f16f13
LeeMinseo0117 <leems3164@naver.com>
2024-09-11 16:04:39 +0900

    투두 작성

--- typescript/react/todo/src/App.jsx
+++ typescript/react/todo/src/App.jsx
@@ -1,12 +1,35 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function Todo() {
   const [todoList, setTodoList] = useState([
     { id: "1", todo: "아침먹기" },
     { id: "2", todo: "점심먹기" },
   ]);
+  const [todoText, setTodoText] = useState("");
+  const handleInput = (e) => {
+    // console.log(e.target.value);
+    const newTodoText = e.target.value;
+    setTodoText(newTodoText);
+    // console.log(todoText);
+  };
+
+  const handleButton = () => {
+    setTodoList((prevTodoList) => {
+      const newTodo = {
+        id: +todoList[todoList.length - 1].id + 1,
+        todo: todoText,
+      };
+      const newTodoList = [...prevTodoList, newTodo];
+      if (newTodoList.length > 5) {
+        alert("5개만 추가 가능");
+        todoList.shift();
+      }
+      return newTodoList;
+    });
+  };
+
   return (
     <div>
       <h1>멋진 투두</h1>
@@ -17,8 +40,10 @@ function Todo() {
           </li>
         ))}
         {/* <li>{todoList[0].todo}</li> */}
-        <input type="text"></input>
-        <button type="button">투두추가</button>
+        <input type="text" value={todoText} onChange={handleInput} />
+        <button type="button" onClick={handleButton}>
+          투두추가
+        </button>
       </ul>
     </div>
   );
