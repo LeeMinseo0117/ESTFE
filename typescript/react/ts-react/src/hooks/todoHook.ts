57b6364844ae877c4b2bf7aae05081a7c64c0233
LeeMinseo0117 <leems3164@naver.com>
2024-09-12 16:06:24 +0900

    async await 사용

--- typescript/react/ts-react/src/hooks/todoHook.ts
+++ typescript/react/ts-react/src/hooks/todoHook.ts
@@ -1,49 +1,54 @@
 import { useEffect, useState } from "react";
-
 export interface TodoItem {
   id: string;
   todo: string;
 }
 
+// 튜플 배열같은애인데 길이랑, 각 위치의 타입이 정해져있어요.
+// let arr:[number,string,number] = [1,'23',1]
 function useTodoData(): [TodoItem[], (todoText: string) => void, boolean] {
   const [isLoading, setIsLoading] = useState(false);
   const [todoList, setTodoList] = useState<TodoItem[]>([]);
-
   useEffect(() => {
     setIsLoading(true);
-    fetch("http://localhost:3000/todos")
-      .then((res) => {
-        return res.json();
-      })
-      .then((todoData) => {
-        setIsLoading(false);
-        setTodoList(todoData);
-      });
-  }, []);
-  const postTodo = (todoText: string): void => {
-    setIsLoading(true);
-    const newTodo = {
-      todo: todoText,
+    const getTodo = async () => {
+      const res = await fetch("http://localhost:3300/todos");
+      const todoData: TodoItem[] = await res.json();
+      return todoData;
     };
 
-    fetch("http://localhost:3000/todos", {
+    const initTodo = async () => {
+      const todoData = await getTodo();
+      setIsLoading(false);
+      setTodoList(todoData);
+    };
+    initTodo();
+  }, []);
+
+  const postTodo = async (newTodo: Partial<TodoItem>): Promise<TodoItem> => {
+    const res = await fetch("http://localhost:3300/todos", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(newTodo),
-    })
-      .then((res) => {
-        return res.json();
-      })
-      .then((newTodo) => {
-        setIsLoading(false);
-        setTodoList((prevTodoList) => {
-          return [...prevTodoList, newTodo];
-        });
-      });
+    });
+    const addedTodo = await res.json();
+    return addedTodo;
   };
-  return [todoList, postTodo, isLoading];
+
+  const addTodo = async (todoText: string): Promise<void> => {
+    setIsLoading(true);
+    const newTodo = {
+      todo: todoText,
+    };
+    const addedTodo = await postTodo(newTodo);
+    setIsLoading(false);
+    setTodoList((prevTodoList) => {
+      return [...prevTodoList, addedTodo];
+    });
+  };
+  return [todoList, addTodo, isLoading];
 }
 
 export default useTodoData;
