4b4dd00b664ba608bd5473ffa5458bfe7bfd0903
LeeMinseo0117 <leems3164@naver.com>
2024-09-19 23:10:46 +0900

    0919 내용 추가

--- firebase/diary/src/components/header/Header.jsx
+++ firebase/diary/src/components/header/Header.jsx
@@ -1,23 +1,45 @@
-import React from "react";
-import styles from "./Header.module.css";
-import { Link } from "react-router-dom";
-import logo from "../../img/logo.svg";
+import React from 'react'
+import styles from './Header.module.css';
+import { Link, useLocation } from 'react-router-dom';
+import logo from '../../img/logo.svg';
+import { useLogout } from '../../hooks/useLogout';
+import { useAuthContext } from '../../hooks/useAuthContext';
 
 export default function Header() {
-  return (
-    <header>
-      <div className={styles["header-wrap"]}>
-        <h1>
-          <Link to="/">
-            <img className={styles.logo} src={logo} alt="두근두근 비밀일기" />
-          </Link>
-        </h1>
-        <div>
-          <Link to="/signup" className="btn-join">
-            회원가입
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
+    const [logout] = useLogout();
+    const { user } = useAuthContext();
+    const location = useLocation();
+
+    return (
+        <header>
+            <div className={styles["header-wrap"]}>
+                <h1>
+                    <Link to="/">
+                        <img className={styles.logo} src={logo} alt="두근두근 비밀일기" />
+                    </Link>
+                </h1>
+                <div>
+                    {!user && (
+                        <>
+                            {
+                                // 유저 정보가 없고, 회원가입 화면이라면 로그인 버튼을, 로그인 화면 이라면 회원가입 버튼을 노출합니다.
+                                location.pathname === "/signup" ? <Link to="/login" className="btn-login">로그인</Link> : <Link to="/signup" className="btn-join">회원가입</Link>
+                            }
+                        </>
+                    )}
+                    {user && (
+
+                        // 유저 정보가 없고, 회원가입 화면이라면 로그인 버튼을, 로그인 화면 이라면 회원가입 버튼을 노출합니다.
+                        <>
+                            <p className="hello">환영합니다 <strong>{user.displayName}</strong>님!</p>
+                            <Link to="/" className="btn-join" onClick={logout}>로그아웃</Link>
+                        </>
+
+                    )}
+
+
+                </div>
+            </div >
+        </header >
+    )
 }
--- firebase/diary/src/hooks/useSignup.js
+++ firebase/diary/src/hooks/useSignup.js
@@ -1,39 +1,53 @@
 import { useState } from "react";
-import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
 import { appAuth } from "../firebase/config";
+import { useAuthContext } from "./useAuthContext";
 
 export const useSignup = () => {
-  // 에러 상태 관리
-  const [error, setError] = useState(null);
-  // 통신 상태 관리
-  const [isPending, setIsPending] = useState(false);
 
-  const signup = (email, password, displayName) => {
-    setIsPending(true);
-    createUserWithEmailAndPassword(appAuth, email, password)
-      .then((userCredential) => {
-        const user = userCredential.user;
-        console.log(user);
-        if (!user) {
-          throw new Error("회원가입에 실패");
-        }
-        updateProfile(appAuth.currentUser, { displayName })
-          .then(() => {
-            setIsPending(false);
-          })
-          .catch((error) => {
-            setError(error);
-            console.error(error);
-            setIsPending(false);
-          });
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        setError(errorMessage);
-        console.error(`에러코드: ${errorCode},${error}`);
-        setIsPending(false);
-      });
-  };
-  return [error, isPending, signup];
-};
+    // 에러상태를 관리합니다.
+    const [error, setError] = useState(null);
+
+    // 통신상태를 관리합니다.
+    const [isPending, setIsPending] = useState(false);
+
+    // 전역 context 에서 dispatch 함수를 받아옵니다.
+    const { dispatch } = useAuthContext();
+
+
+    const signup = (email, password, displayName) => {
+        setIsPending(true);
+
+        createUserWithEmailAndPassword(appAuth, email, password)
+            .then((userCredential) => {
+                // 현재 회원가입을 통해 로그인한 유저의 정보
+                const user = userCredential.user;
+
+                if (!user) {
+                    throw new Error('회원가입에 실패했습니다.');
+                }
+
+                updateProfile(appAuth.currentUser, { displayName })
+                    .then(() => {
+                        dispatch({ type: 'login', payload: user });
+                        setIsPending(false);
+                        setError(null);
+                    }).catch((error) => {
+                        setError(error);
+                        console.error(error);
+                        setIsPending(false);
+                    });
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                setError(errorMessage);
+                console.error(`에러코드 : ${errorCode}. ${error}`);
+                setIsPending(false);
+            });
+
+    }
+
+    return [error, isPending, signup]
+
+}
